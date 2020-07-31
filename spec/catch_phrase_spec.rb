d377ab525f8d7f9114734746930b4c1c3b959545
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-07-31 04:14:24 +0000

    Automatically backed up by Learn

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require "spec_helper"
 
 describe "#catch_phrase" do 
@@ -6,3 +7,13 @@ describe "#catch_phrase" do
     expect{catch_phrase(phrase)}.to output("It's-a me, Mario!\n").to_stdout
   end
 end
+=======
+require "spec_helper"
+
+describe "#catch_phrase" phrase 
+  it "puts out a catch phrase" phrase 
+    phrase = "It's-a me, Mario!"
+    expect{catch_phrase}.to output("It's-a me, Mario!\n").to_stdout
+  end
+end
+>>>>>>> 67bd869c1d1c457767e463f96310ca820bfb0b8d
